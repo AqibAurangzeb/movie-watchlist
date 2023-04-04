a3d4e6d5198ea97755a00c1db56997f74983604c
AqibAurangzeb <aqibaurangzebdeveloper@gmail.com>
2023-04-04 13:36:36 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ const search = document.getElementById('search')
 const movies = document.getElementById('movies')
 const startExploring = document.getElementById('start-exploring')
 
-const apiKey = process.env.OMDB_API_KEY // injected in from env variables
+const apiKey = '8da3f7ed'
 let searchResults = []
 let watchlist = JSON.parse(localStorage.getItem("watchlist")) ?? []
 
